26a63f878c97b0a355832e48c58bbf6d308821a8
Uwakmfon-Ibanga <ibangauwakmfon200@gmail.com>
2025-05-22 15:56:39 +0100

    imported route and routes from react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import './App.css'
+import { Routes, Route } from "react-router-dom";
 import CamperRegestration from "./pages/CamperRegestration";
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<CamperRegestration />} />  
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<CamperRegestration />} />  
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
