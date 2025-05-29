7c9071ab7a168ee6edb8e73ca1beda8fa159a04d
Uwakmfon-Ibanga <ibangauwakmfon200@gmail.com>
2025-05-29 22:53:41 +0100

    changed home route

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<CamperRegestration />} />  
+        <Route path="/" element={<CamperRegestration />} />
+        {/* <Route path="/registration" element={<CamperRegestration />} />   */}
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
